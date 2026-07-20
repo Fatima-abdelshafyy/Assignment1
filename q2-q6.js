ff4f5369f975b86cb0233ff35293346c358adc4b
fatima <fatima.abdelshafyy@gmail.com>
2026-07-20 20:12:38 +0300

    q2-q5

--- q2-q6.js
+++ q2-q6.js
@@ -1,5 +1,5 @@
-console.log(window)//-------------Q2--------------
-let value=0;
+//-------------Q2--------------
+let value = 0;
 
 if (!value) {
     console.log("Invalid");
@@ -8,16 +8,26 @@ if (!value) {
 }//Invalid
 
 //-------------Q3--------------
- for(let i=1;i<=10;i++){
-    if(i%2==0){
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
         continue;
-    }else{
+    } else {
         console.log(i)
     }
 }//1 3 5 7 9*/
 //-------------Q4--------------
 let arr = [1, 2, 3, 4, 5, 6];
-let evenArr=arr.filter((element) => {
-     return element%2==0;
+let evenArr = arr.filter((element) => {
+    return element % 2 == 0;
 });
-console.log(evenArr);
+console.log(evenArr);//[2,4,6]
+//-------------Q5--------------
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+let merged = [
+    ...arr1,
+    ...arr2
+];
+console.log(merged);//[ 1, 2, 3, 4, 5, 6 ]
+//-------------Q6--------------
+
